868ddeb5e30bfd8488b268ff7a431a8ab9d67ba5
sourcreamsource <rovotev@gmail.com>
2026-06-09 18:39:12 +0900

    feat: modify function and file rename

---
+++ practice-notes/team/calculator_rename.py
@@ -0,0 +1,19 @@
+# calculator.py
+
+def calculate(a, b):
+    result = a + b
+    return result
+
+
+print("Calculator Program Start!")
+print(calculate(2, 122))
+
+# 이것은 마이너스다!!! 마이너스 없애버림
+
+
+# 곱하기 만듬
+def multiple(f, u):
+    result = f * u
+    return result
+    
+print(multiple(2, 122))
