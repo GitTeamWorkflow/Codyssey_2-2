1a1a9be29978f877455976fc61e0ec33a1b1a458
Lim Jonghan <jonghan0330@gmail.com>
2026-06-11 13:59:58 +0900

    [feat] 나누기 추가

--- practice-notes/team/calculator_lee_lim.py
+++ practice-notes/team/calculator_lee_lim.py
@@ -1,23 +1,27 @@
 # calculator.py
 
+# 더하기
 def calculate(a, b):
     result = a + b
     return result
-
-
-print("Calculator Program Start!")
-print(calculate(2, 122))
 
 # 빼기
 def minus(a, b):
     result = a - b
     return result
-    
-print(minus(2, 122))
 
 # 곱하기
 def multiple(f, u):
     result = f * u
     return result
-    
+
+# 나누기
+def divide(d, i):
+    result = d / i
+    return result
+
+print("Calculator Program Start!")
+print(calculate(2, 122))
+print(minus(2, 122))
 print(multiple(2, 122))
+print(divide(2, 122))
